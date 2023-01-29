4987c6d90ca490d50b5f1f4a3705a91615211689
nopetoyou1111 <108734462+nopetoyou1111@users.noreply.github.com>
2023-01-29 15:30:24 +0330

    update main.cpp 🗃️

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     system("chmod 777 scripts/install.sh");
     system("sudo ./scripts/install.sh");
     system("clear");
+    std::cout << "\033[0;32mMMMMMMMMMMMMMMMMMNd::coxOXWMMMMMMMMMMMMM\nMMMMMMMMMMMMMMMMM0'      .;o0WMMMMMMMMMM\nMMMMMMMMMMMMMMMMWx.        .dWMMMMMMMMMM\nMMMMMMMMMMMMMMWKk:...     .oNMMMMMMMMMMM\nMMMMMMMMMMMMMMXl. ..',::,.lXMMMMMMMMMMMM\nMMMMMMMMMMMMMMWd.     .,lcckKXWMMMMMMMMM\nMMMMMMMMMMMMMNKc         ;l,.':OWMMMMMMM\nMMMMMWWWWWWMWx'.         ;0o   ,0MMMMMMM\nMMMMNdlxooKMMd.          .dKl. .oKWMMMMM\nMMMMWkoOodNN0c          .lXWk.   'OWMMMM\nMMMMM0dOo:c,..,.         ,d0Kc    ,0MMMM\nMMMMM0dOl..',,c;,'.,c,''   .:l.    :XMMM\nMMMMM0dOd:'.;:c,,:'ck;':.           oNMM\nMMMMM0dOl;c;',:'...';.',.           .kWM\nMMWKOddk:;kl..;d:.  .';,''.          ;KM\nMMNxclxkolkc  .ox;...',,,,.... .. ...,kW\nMMMMWWWM0oko.'::':,,dc';:cc.;l:,,';l,,xW\nMMMMMMMM0okdcoko':;,dc'',:c.':c;..;:.:XM\nMMMMMMMM0okdkKxxkc.,oc,c:;c,o0OkkxxkkKMM\nMMMMMMMNo:dccKNOxdc;,,;xOkkOXMMMMMMMMMMM\nMMMMMMMNxloodKMWKxdoodONMMMMMMMMMMMMMMMM";
     std::cout <<  "\033[0;32m Hello from Hack it (:\n";
     std::cout <<  " \033[1;31mGithub : https://github.com/nopetoyou1111/HACKit\n";
     // std::cin >> ans;
@@ -24,14 +25,18 @@ int main() {
             system("chmod 777 tools/John-cli/install.sh");
             system("sudo bash tools/John-cli/install.sh");
             system("sudo python3 ./tools/John-cli/john.py");
+            return 0;
+
 
         }
         else if(yn == "n" || yn == "N"  || yn == "No"  || yn == "no"  || yn == "NO"  || yn == "Nope"  || yn == "NOPE" || yn == "nope"){
             system("[!]NEVER GONNA SAY GOODBYE (:");
+            return 0;
         }
     }
 
 
 }while (true);
+return 0;
 
 }
