a4c9214ff9d0d9e2b8d3de9108e7b579082a0b25
nopetoyou1111 <108734462+nopetoyou1111@users.noreply.github.com>
2023-01-29 15:32:40 +0330

    install files 🏠

--- tools/John-cli/install.sh
+++ tools/John-cli/install.sh
@@ -1,10 +1,4 @@
-GRY='\033[1;30m'
 RED='\033[0;31m'
-BLU='\033[0;34m'
-GRN='\033[0;32m'
-PUL='\033[0;35m'
-RST='\033[0m'
-
 checkroot() {
     SAVE_LD_PRELOAD="$LD_PRELOAD"
     unset LD_PRELOAD
@@ -15,14 +9,7 @@ checkroot() {
     LD_PRELOAD="$SAVE_LD_PRELOAD"
 }
 checkroot
-
 install(){
     sudo apt-get install john
-    
-}
-run(){
-    make tools/John-cli/john
-    sudo ./tools/John-cli/john
 }
 install
-# run
